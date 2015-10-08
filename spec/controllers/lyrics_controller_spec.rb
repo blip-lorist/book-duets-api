8de012eff496869fe03da8e9b74fd2c5280b97be
Loraine <loraine@lorainekv.com>
2015-10-08 12:08:13 -0700

    Started on the lyrics_collection spec.

--- spec/controllers/lyrics_controller_spec.rb
+++ spec/controllers/lyrics_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
+require 'vcr_setup'
 
 RSpec.describe LyricsController, type: :controller do
 
+  describe "GET #lyric_collection" do
+
+    it "retrieves Bjork lyrics" do
+      VCR.use_cassette 'controller/lyric_collection' do
+        get :lyric_collection, params: {}
+        expect(response.response_code).to eq(200)
+      end
+    end
+  end
 end
