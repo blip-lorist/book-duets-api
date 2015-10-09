2b6bd5d8f02354719dd85beedbe6cbcd6d47999f
Loraine <loraine@lorainekv.com>
2015-10-08 19:02:23 -0700

    Wrote a collect_tracks method with nickelback hardcoded in.

--- app/controllers/lyrics_controller.rb
+++ app/controllers/lyrics_controller.rb
@@ -1,7 +1,23 @@
 class LyricsController < ApplicationController
 
-  def get_artist
+  BASE_URI = "http://api.musixmatch.com/ws/1.1/"
 
+
+private
+
+  def collect_tracks
+  # @artist_name = params[:artist]
+  response = HTTParty.get(BASE_URI + "track.search?q_artist=nickelback&f_has_lyrics=1&page_size=5&format=json&apikey=#{ENV['MUSIX_MATCH']}")
+  json_response = JSON.parse(response)
+  tracks = json_response["message"]["body"]["track_list"]
+
+  track_ids = []
+
+  tracks.each do |track|
+    track_ids << track["track"]["track_id"]
+  end
+
+  return track_ids
   end
 
 end
