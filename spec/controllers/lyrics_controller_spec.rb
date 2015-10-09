c02c062fcc2e1ce41045d3f3ffe388530636dbc2
Loraine <loraine@lorainekv.com>
2015-10-09 13:10:01 -0700

    Wrote a spec that checks lyrical_corpus for dirty data.

--- spec/controllers/lyrics_controller_spec.rb
+++ spec/controllers/lyrics_controller_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe LyricsController, type: :controller do
   describe "get_lyrics" do
     it "collects lyrics in a txt file" do
       VCR.use_cassette 'controller/get_lyrics' do
-        lyrics = (controller.send(:get_lyrics))
+        controller.send(:get_lyrics)
 
         expect(File).to exist("lyrical_corpus.txt")
         expect(File.zero?("lyrical_corpus.txt")).to be(false)
@@ -27,4 +27,13 @@ RSpec.describe LyricsController, type: :controller do
     end
   end
 
+  describe "clean_lyrics" do
+    it "removes non-lyrical content from the corpus" do
+      # controller.send(:clean_lyrics)
+      corpus = File.open("lyrical_corpus.txt")
+
+      expect(corpus.read).to_not include("******* This Lyrics is NOT for Commercial use *******")
+      corpus.close
+    end
+  end
 end
