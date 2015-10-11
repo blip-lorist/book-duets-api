62babbe7aa7384eec9bfc91ee38f01ec29a692ae
Loraine <loraine@lorainekv.com>
2015-10-11 14:57:30 -0700

    Moved all build_lyrics_corpus specs into the helper spec.

--- spec/helpers/book_duets_helper_spec.rb
+++ spec/helpers/book_duets_helper_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-
+require 'vcr_setup'
 # Specs in this file have access to a helper object that includes
 # the BookDuetsHelper. For example:
 #
@@ -11,5 +11,40 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe BookDuetsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "collect_tracks" do
+    it "retrieves five track_ids for Nickelback" do
+      VCR.use_cassette 'helper/artist_collection' do
+        tracks = (helper.send(:collect_tracks))
+
+        expect(tracks).to be_an_instance_of Array
+        expect(tracks.length).to be(5)
+      end
+    end
+  end
+
+  describe "get_lyrics" do
+    it "collects lyrics in a txt file" do
+      VCR.use_cassette 'helper/get_lyrics' do
+        helper.send(:get_lyrics)
+
+        expect(File).to exist("lyrical_corpus.txt")
+        expect(File.zero?("lyrical_corpus.txt")).to be(false)
+        # Ensures that the file isn't empty. Difficult to test actual
+        # content since it's randomly selected
+      end
+    end
+  end
+
+  describe "clean_lyrics" do
+    it "removes non-lyrical content from the corpus" do
+      helper.send(:clean_lyrics)
+      corpus = File.open("lyrical_corpus.txt")
+
+      expect(corpus.read).to_not include("******* This Lyrics is NOT for Commercial use *******")
+      expect(corpus.read).to_not include("...")
+
+      corpus.close
+    end
+  end
 end
