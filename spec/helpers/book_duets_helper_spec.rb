87343c5137aa0983c8f1909c7aa32d997e397e0b
Loraine <loraine@lorainekv.com>
2015-10-12 11:46:59 -0700

    Wrote spec suite for literary_corpus.

--- spec/helpers/book_duets_helper_spec.rb
+++ spec/helpers/book_duets_helper_spec.rb
@@ -12,39 +12,67 @@ require 'vcr_setup'
 # end
 RSpec.describe BookDuetsHelper, type: :helper do
 
-  describe "collect_tracks" do
-    it "retrieves five track_ids for Nickelback" do
-      VCR.use_cassette 'helper/artist_collection' do
-        tracks = (helper.send(:collect_tracks))
+  context "building a lyrical corpus" do
+    describe "collect_tracks" do
+      it "retrieves five track_ids for Nickelback" do
+        VCR.use_cassette 'helper/artist_collection' do
+          tracks = (helper.send(:collect_tracks))
 
-        expect(tracks).to be_an_instance_of Array
-        expect(tracks.length).to be(5)
+          expect(tracks).to be_an_instance_of Array
+          expect(tracks.length).to be(5)
+        end
+      end
+    end
+
+    describe "get_lyrics" do
+      it "collects lyrics in a txt file" do
+        VCR.use_cassette 'helper/get_lyrics' do
+          helper.send(:get_lyrics)
+
+          expect(File).to exist("lyrical_corpus.txt")
+          expect(File.zero?("lyrical_corpus.txt")).to be(false)
+          # Ensures that the file isn't empty. Difficult to test actual
+          # content since it's randomly selected
+        end
+      end
+    end
+
+    describe "clean_lyrics" do
+      it "removes non-lyrical content from the corpus" do
+        helper.send(:clean_lyrics)
+        corpus = File.open("lyrical_corpus.txt")
+
+        expect(corpus.read).to_not include("******* This Lyrics is NOT for Commercial use *******")
+        expect(corpus.read).to_not include("...")
+
+        corpus.close
       end
     end
   end
 
-  describe "get_lyrics" do
-    it "collects lyrics in a txt file" do
-      VCR.use_cassette 'helper/get_lyrics' do
-        helper.send(:get_lyrics)
+  context "building a literary corpus" do
+    describe "get_lit" do
+      it "collects literary quotes in a txt file" do
+        VCR.use_cassette "helper/get_lit" do
+          helper.send(:get_lit)
 
-        expect(File).to exist("lyrical_corpus.txt")
-        expect(File.zero?("lyrical_corpus.txt")).to be(false)
-        # Ensures that the file isn't empty. Difficult to test actual
-        # content since it's randomly selected
+          expect(File).to exist("literary_corpus.txt")
+          expect(File.zero?("literary_corpus.txt")).to be(false)
+        end
       end
     end
-  end
 
-  describe "clean_lyrics" do
-    it "removes non-lyrical content from the corpus" do
-      helper.send(:clean_lyrics)
-      corpus = File.open("lyrical_corpus.txt")
+    describe "clean_lit" do
+      it "removes unrelated content from the corpus" do
+        helper.send(:clean_lit)
+        corpus = File.open("literary_corpus.txt")
 
-      expect(corpus.read).to_not include("******* This Lyrics is NOT for Commercial use *******")
-      expect(corpus.read).to_not include("...")
+        expect(corpus.read).to_not include('\\n')
+        expect(corpus.read).to_not include("<")
+        expect(corpus.read).to_not include("  ")
 
-      corpus.close
+        corpus.close
+      end
     end
   end
 end
