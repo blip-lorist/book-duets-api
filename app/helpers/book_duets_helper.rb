e0a8f6cbf1774a2c9edb8a23a6e18e097e69bf81
Loraine <loraine@lorainekv.com>
2015-10-13 13:19:45 -0700

    Realized that my lyrical_corpus methods shouldn't be in my helper, so I extracted it to a lib file.

--- app/helpers/book_duets_helper.rb
+++ app/helpers/book_duets_helper.rb
@@ -6,14 +6,9 @@ module BookDuetsHelper
 # API sandbox link
 # https://en.wikipedia.org/wiki/Special:ApiSandbox
 
-  LYRICS_BASE_URI = "http://api.musixmatch.com/ws/1.1/"
   LIT_BASE_URI = "https://en.wikiquote.org/w/api.php?"
 
-  def build_lyrical_corpus (musician)
-    collect_random_tracks
-    get_lyrics
-    clean_lyrics
-  end
+
 
   def build_literary_corpus (author)
     get_lit
@@ -22,59 +17,7 @@ module BookDuetsHelper
 
   private
 
-  # ____ LYRIC METHODS ____ #
-  def collect_random_tracks
-    # artist_name = params[:artist]
-    response = HTTParty.get(LYRICS_BASE_URI + "track.search?q_artist=nickelback&f_has_lyrics=1&page_size=20&format=json&apikey=#{ENV['MUSIX_MATCH']}")
-    json_response = JSON.parse(response)
-    tracks = json_response["message"]["body"]["track_list"]
-
-    track_ids = []
-    tracks.each do |track|
-      track_ids << track["track"]["track_id"]
-    end
-
-    random_tracks = track_ids.shuffle!.take(5)
-
-    return random_tracks
-  end
-
-  def get_lyrics
-    lyrical_corpus = open("lyrical_corpus.txt", "a")
-    #Remove old lyrics
-    lyrical_corpus.truncate(0)
-
-    track_ids = collect_random_tracks
-
-    track_ids.each do |id|
-      response = HTTParty.get(LYRICS_BASE_URI + "track.lyrics.get?track_id=#{id}&apikey=#{ENV['MUSIX_MATCH']}")
-      json_response = JSON.parse(response)
-      lyrics = json_response["message"]["body"]["lyrics"]["lyrics_body"]
-      lyrical_corpus << lyrics
-    end
-    lyrical_corpus.close
-  end
-
-  def clean_lyrics
-
-    original_corpus = open("lyrical_corpus.txt", "r")
-    clean_corpus = open("lyrical_corpus_temp.txt", "a")
-    lyrics = original_corpus.read
-
-    # Cleaning the lyrics
-    lyrics.gsub!("******* This Lyrics is NOT for Commercial use *******", "")
-    lyrics.gsub!("...", "")
-    clean_corpus.write(lyrics)
-    # TODO: Obscenity filter? Or should this happen at the very end of the mashup?
-    original_corpus.close
-    clean_corpus.close
-
-    # The original lyrical corpus is now a backup file
-    File.rename("lyrical_corpus.txt", "lyrical_corpus.bak")
-    # The clean lyrical corpus becomes the main corpus file
-    File.rename("lyrical_corpus_temp.txt", "lyrical_corpus.txt")
-  end
-
+  
   # ____ LIT METHODS ____ #
 
   # Collect Wikiquote sections
