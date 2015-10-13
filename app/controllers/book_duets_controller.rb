c73a2511774c781829be94a327cbe94707415f87
Loraine <loraine@lorainekv.com>
2015-10-13 11:56:24 -0700

    WIP spec and method for custom_duet.

--- app/controllers/book_duets_controller.rb
+++ app/controllers/book_duets_controller.rb
@@ -1,7 +1,8 @@
 class BookDuetsController < ApplicationController
 
   def custom_duet
-
+    duos = request.parameters
+    render json: {duo_info: duos}, status: :ok
   end
 
 end
--- spec/controllers/book_duets_controller_spec.rb
+++ spec/controllers/book_duets_controller_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe BookDuetsController, type: :controller do
   describe "GET #custom_duet" do
     it "is successful" do
       get :custom_duet, duo: {author: "Neil Gaiman", musician: "Nickelback"}
+
+      expect(response.response_code).to eq(200)
     end
   end
 end
