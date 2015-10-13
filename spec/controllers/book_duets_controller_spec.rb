01436c209b6d4385eb256c325e78c76c8a1b1e72
Loraine <loraine@lorainekv.com>
2015-10-13 11:25:59 -0700

    WIP spec for custom_duet endpoint and controller action.

--- spec/controllers/book_duets_controller_spec.rb
+++ spec/controllers/book_duets_controller_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe BookDuetsController, type: :controller do
 
+  describe "GET #custom_duet" do
+    it "is successful" do
+      get :custom_duet, duo: {author: "Neil Gaiman", musician: "Nickelback"}
+    end
+  end
 end
