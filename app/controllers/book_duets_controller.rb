04f6bcb175f2eb073a077a2ff24a48426e46be07
Loraine <loraine@lorainekv.com>
2015-10-13 13:20:20 -0700

    Lyrical corpus is now build up dynamically. Whooo!

--- app/controllers/book_duets_controller.rb
+++ app/controllers/book_duets_controller.rb
@@ -1,7 +1,14 @@
 class BookDuetsController < ApplicationController
 
+  require "./lib/lyrical_corpus"
+
   def custom_duet
     duo = request.parameters
+
+    musician = duo["musician"]
+
+    LyricalCorpus.new.build_lyrical_corpus(musician)
+
     render json: {author: duo["author"], musician: duo["musician"]}, status: :ok
   end
 
