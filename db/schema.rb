5c39d48236699ec6a0e9ec53ade6a799bff50d29
Loraine <loraine@lorainekv.com>
2015-10-13 23:47:29 -0700

    Created a BookDuet model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151014063119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "book_duets", force: :cascade do |t|
+    t.string   "musician"
+    t.string   "author"
+    t.string   "news_source"
+    t.string   "persisted_dictionary"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
 
 end
