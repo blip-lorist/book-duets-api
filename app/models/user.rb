11b746a22ba985aee311337bd2e56a767cea3fad
Loraine <loraine@lorainekv.com>
2015-10-18 23:05:30 -0700

    Added some validation notes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   before_create :generate_api_key
 
+  #Validations
+    # Emails must be unique
+    # keys must be unique
+
   def generate_api_key
     self.api_key = SecureRandom.base64.tr('+/=', 'xpq')
   end
