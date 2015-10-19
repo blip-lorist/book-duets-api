c97b4b9b48d9efe8bb16821cfff6b261e5589d20
Loraine <loraine@lorainekv.com>
2015-10-18 23:06:23 -0700

    TODO note.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     user = User.find_by(email:email)
     api_key = user.api_key
 
+    # TODO: I need error handling here in case sign up fails
     render json: {message: "Here is your unique API key.", api_key: api_key }
   end
 end
