cf146097a4aa1e6e2a68f9bd38744a68af4c869c
Loraine <loraine@lorainekv.com>
2015-10-18 23:05:42 -0700

    Wrote a very basic new user method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
 
   def new
-    
+    email = params["email"]
 
+    User.create(email:email)
+    user = User.find_by(email:email)
+    api_key = user.api_key
+
+    render json: {message: "Here is your unique API key.", api_key: api_key }
   end
 end
