d7b0e1007cbce31a463fcc30ab9a5947138d5733
Loraine <loraine@lorainekv.com>
2015-10-22 23:21:54 -0700

    Updated redis initializer for heroku.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,4 +2,7 @@ if Rails.env.test?
   $redis = MockRedis.new
 elsif Rails.env.development?
   $redis = Redis.new(:host => 'localhost', :port => 6379)
+elsif Rails.env.production?
+  $redis = Redis.new(:host => ENV['REDIS_URL'],
+  :port => 17319)
 end
