ca60df7efe282c9e3a0e5387d42fbbbbec5941ad
Loraine <loraine@lorainekv.com>
2015-10-25 00:19:11 -0700

    Language filtering dictionaries and initializer.

---
+++ config/initializers/language_filter.rb
@@ -0,0 +1,8 @@
+@safe_filter = LanguageFilter::Filter.new()
+@safe_filter.matchlist = File.join(Rails.root,"/dictionaries/nonos.yml")
+@safe_filter.replacement = :garbled
+
+
+@edgy_filter = LanguageFilter::Filter.new()
+@edgy_filter.matchlist = File.join(Rails.root,"/dictionaries/edgy_nonos.yml")
+@edgy_filter.replacement = :garbled
