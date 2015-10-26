a81cfc3dda4424c5db79232f67263fddf9548e76
Loraine <loraine@lorainekv.com>
2015-10-25 18:55:14 -0700

    Made the language filters globally available.

--- config/initializers/language_filter.rb
+++ config/initializers/language_filter.rb
@@ -3,6 +3,6 @@ $safe_filter.matchlist = File.join(Rails.root,"/dictionaries/nonos.yml")
 $safe_filter.replacement = :garbled
 
 
-@edgy_filter = LanguageFilter::Filter.new()
-@edgy_filter.matchlist = File.join(Rails.root,"/dictionaries/edgy_nonos.yml")
-@edgy_filter.replacement = :garbled
+$edgy_filter = LanguageFilter::Filter.new()
+$edgy_filter.matchlist = File.join(Rails.root,"/dictionaries/edgy_nonos.yml")
+$edgy_filter.replacement = :garbled
